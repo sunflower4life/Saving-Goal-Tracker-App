b176a323c33f507908a0e7015abd6a2ea52c1847
nadh <nrlndh04@gmail.com>
2025-10-26 13:34:54 +0800

    update UI

--- lib/sgtCalcScreen.dart
+++ lib/sgtCalcScreen.dart
@@ -9,16 +9,14 @@ class SGTCalcScreen extends StatefulWidget {
 
 class _SGTCalcScreenState extends State<SGTCalcScreen> {
 
-  String typeSaving = 'Weekly';//for dropdown of type saving(weekly/daily)
-
   //untuk retrieve user input drp textfields
   TextEditingController targetAmountController = TextEditingController();
   TextEditingController savingPerWeekController = TextEditingController();
   TextEditingController startingAmountController = TextEditingController();
 
   //variable to store calc results, initially 0
-  double weeksNeeded =0;
-  double monthsNeeded =0;
+  int weeksNeeded =0;
+  int daysNeeded = 0;
 
   // FocusNode to set focus back to first field after reset
   FocusNode targetFocusNode = FocusNode();
@@ -27,188 +25,173 @@ class _SGTCalcScreenState extends State<SGTCalcScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Saving Goal Tracker" , style: TextStyle(color: Colors.black),),
-        backgroundColor: const Color.fromARGB(255, 117, 187, 217),
+        title: Text("Saving Goal Tracker" , 
+        style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: const Color.fromARGB(232, 240, 215, 161),
       ),
-      body:Center(
-        child: SingleChildScrollView(
-          child: Container(
-            //padding: const EdgeInsets.fromLTRB(20, 16, 0 , 100),
-            padding: const EdgeInsets.all(16.0),
-            margin: const EdgeInsets.all(16.0),
-            //margin: const EdgeInsets.fromLTRB(16, 80, 16, 80),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 234, 229, 229),
-            ),
-            
-            width: 350,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Calculate Your Saving Goal' , 
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: const Color.fromARGB(255, 42, 76, 136),
-                ),
-                ),
-                SizedBox(height: 25),
-                Row(
-                  children: [
-                    //User enter starting amount
-                    SizedBox(width: 130, child: Text('Target Amount')),//width 130 jarak antara kotak dengan title(text)
-                    SizedBox(
-                      width: 170,
-                      child: TextField(
-                        controller: targetAmountController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter Target(RM)',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    //User enter starting amount
-                    SizedBox(width: 130, child: Text('Starting Amount')),//width 130 jarak antara kotak dengan title(text)
-                    SizedBox(
-                      width: 170,
-                      child: TextField(
-                        controller: startingAmountController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter Starting(RM)',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 5),
-                Row(
-                  children: [
-                    const SizedBox(width:130, child: Text('Type Saving')),
-                    DropdownButton<String>(
-                      value: typeSaving,
-                      items: <String>['Weekly','Monthly'].map((String value){
-                        return DropdownMenuItem<String>(
-                          value:value,
-                          child: Text(value) ,
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue){
-                        typeSaving = newValue!;
-                        setState(() {});
-                      }
-                    ),
-                  ],
-                ),
-                SizedBox(height:10),
-                Row(
-                  children: [
-                    //User enter amount saving/week
-                    SizedBox(width: 130, child: Text('Saving Amount')), //width 130 jarak antara kotak dengan title(text)
-                    SizedBox(
-                      width: 170,
-                      child: TextFormField(
-                        controller: savingPerWeekController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter Saving(RM)',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                //Calculate and reset button
-                SizedBox(height:10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(width:10),
-                    ElevatedButton(
-                      onPressed: calculateSGT, 
-                      /*style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 117, 187, 217),
-                        foregroundColor: Colors.white,
-                      ),*///call function
-                      child: Text('Calculate'), 
-                    ),
-                    SizedBox(height:20 ,width:20),
-                    ElevatedButton(
-                      onPressed: () {
-                        //clear all input
-                        savingPerWeekController.clear();
-                        targetAmountController.clear();
-                        startingAmountController.clear();
-
-                        //reset dropdownbutton to default
-                        typeSaving = 'Weekly';
-
-                        //set focus back to first field 
-                        FocusScope.of(context).requestFocus(targetFocusNode);
-                        targetFocusNode.requestFocus();
-
-                        //reset all variable
-                        weeksNeeded =0;
-                        monthsNeeded=0;
-
-                        setState((){});
-                      },
-                      /*style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 117, 187, 217),
-                        foregroundColor: Colors.white,
-                      ),*/
-                      child:Text('Reset'),
-                    ),
-                  ],
-                ),
-                //untuk display result
-                //SizedBox(height:10),
-                SizedBox(height: 10),
-                Container(
-                  //width: double.infinity,
-                  margin: const EdgeInsets.all(16.0),
-                  padding: EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(164, 131, 151, 161),
+      body:Container(
+        color: const Color.fromARGB(255, 247, 237, 215),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(20.0),
+              width: 350,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.white70,
+                //color: const Color.fromARGB(255, 234, 229, 229),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //header title
+                  Text('Calculate Your Saving Goal' , 
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color:Colors.brown,
                   ),
-                  child: Column(children: [
-                    const Text(
-                      'Result:',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 28, 26, 149),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    children: [
+                      //User enter starting amount
+                      SizedBox(width: 130, child: Text('Target Amount')),//width 130 jarak antara kotak dengan title(text)
+                      SizedBox(
+                        width: 170,
+                        child: TextField(
+                          controller: targetAmountController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter Target(RM)',
+                          ),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    //Display with 2 decimal places
-                    Text(
-                      'Weeks Needed: ${weeksNeeded.toStringAsFixed(2)} weeks ',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                    ],
+                  ),
+
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      //User enter starting amount
+                      SizedBox(width: 130, child: Text('Starting Amount')),//width 130 jarak antara kotak dengan title(text)
+                      SizedBox(
+                        width: 170,
+                        child: TextField(
+                          controller: startingAmountController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter Starting(RM)',
+                          ),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Months Needed: ${monthsNeeded.toStringAsFixed(2)} months',
-                        style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                    ],
+                  ),
+                  SizedBox(height:10),
+                  Row(
+                    children: [
+                      //User enter amount saving/week
+                      SizedBox(width: 130, child: Text('Weekly Saving')), //width 130 jarak antara kotak dengan title(text)
+                      SizedBox(
+                        width: 170,
+                        child: TextField(
+                          controller: savingPerWeekController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter Saving(RM)',
+                          ),
+                        ),
                       ),
+                    ],
+                  ),
+                  //Calculate and reset button
+                  SizedBox(height:10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(width:10),
+                      ElevatedButton(
+                        onPressed: calculateSGT, 
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(232, 240, 215, 161),
+                          foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+                        ),//call function
+                        child: Text('Calculate Saving Goal'), 
+                      ),
+                    ],
+                  ),
+                  SizedBox(height:10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          //clear all input
+                          savingPerWeekController.clear();
+                          targetAmountController.clear();
+                          startingAmountController.clear()
+
+                          //set focus back to first field 
+                          FocusScope.of(context).requestFocus(targetFocusNode);
+                          targetFocusNode.requestFocus();
+
+                          //reset all output variable
+                          weeksNeeded =0;
+                          daysNeeded = 0;
+
+                          setState((){});
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(232, 240, 215, 161),
+                          foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+                        ),
+                        child:Text('Reset'),
+                        
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  //result display container
+                  Container(
+                    margin: const EdgeInsets.all(16.0),
+                    padding: EdgeInsets.all(16.0),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: const Color.fromARGB(255, 114, 84, 19)),
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: const Color.fromARGB(255, 240, 222, 168),
                     ),
-                  ],),
-                ),
-              ],
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Time Needed:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 11, 11, 11),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        //Display with 2 decimal places
+                        Text(
+                          '$weeksNeeded weeks and $daysNeeded days',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -216,86 +199,59 @@ class _SGTCalcScreenState extends State<SGTCalcScreen> {
     );
   }
   void calculateSGT(){
+    // Parse inputs using double.tryParse with ?? 0
     double target = double.tryParse(targetAmountController.text)??0;
     double starting = double.tryParse(startingAmountController.text) ?? 0; 
     double weekSaving = double.tryParse(savingPerWeekController.text) ?? 0;
-    //String errorMessage = '';
 
     //Input validatin 1: Check if field empty for each text
-    if(target==0){
+    if (target == 0 || weekSaving == 0) {
+      // Show SnackBar for empty fields
       SnackBar snackBar = const SnackBar(
-        content: Text('Please enter target amount.')
+        content: Text('Please fill in all fields'),
+        backgroundColor: Colors.orange,
+        duration: Duration(seconds: 2),
       );
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      //update ui =0
+
+      // Reset result to 0
       setState(() {
-        weeksNeeded=0;
-        monthsNeeded=0;
+        weeksNeeded = 0;
+        daysNeeded = 0;
       });
-      return;
-    }
-    if(starting ==0){
-      SnackBar snackBar = const SnackBar(
-        content: Text('Please enter starting amount.')
-      );
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      //update ui =0
-      setState(() {
-        weeksNeeded=0;
-        monthsNeeded=0;
-      });
-      return;
-    }
-    if(weekSaving ==0){
-      SnackBar snackBar = const SnackBar(
-        content: Text('Please enter saving.')
-      );
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      //update ui =0
-      setState(() {
-        weeksNeeded=0;
-        monthsNeeded=0;
-      });
-      return;
+      return; // Stop execution if validation fails
     }
     
     //Input validation 2: Check if value is -ve @ buat satu2
     if(target.isNegative || starting.isNegative || weekSaving.isNegative){
       SnackBar snackBar = const SnackBar(
-        content: Text('Please enter positive value.')
+        content: Text('Please enter positive value.'),
+        backgroundColor: Colors.red,
+        duration: Duration(seconds: 2),
       );
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      //update ui =0
+      //update ui, result set to 0
       setState(() {
         weeksNeeded=0;
-        monthsNeeded=0;
+        daysNeeded = 0;
       });
       return;
     }
 
-     //calculate amount to be saved
+     //calculate amount to be saved, how much money still needed
     double amountNeed = target - starting;
-    //adjust saving/week base on type saving
-    double savingPerWeek = weekSaving;
 
-    if(typeSaving == 'Weekly'){ //no change
-      savingPerWeek = weekSaving;
-    } else if (typeSaving == 'Monthly'){
-      savingPerWeek = weekSaving/4;
-    }
-    //calc no of week needed to reach goal
-    double weeks = amountNeed/savingPerWeek;
+    //calc no of week needed to reach goal, week = amount needed / money saved/ week
+    double totalWeeks = amountNeed/weekSaving;
 
-    //convert week to month
-    double months = weeks/4;
-
-    double finalWeeks = double.parse(weeks.toStringAsFixed(2));
-    double finalMonths = double.parse(months.toStringAsFixed(2));
-
+    //seperate into weeks and days
+    int fullWeeks = totalWeeks.floor(); //get whole no (2.5 = 2)
+    double remainingWeeks = totalWeeks - fullWeeks; //get decimal
+    int extraDays = (remainingWeeks * 7).round(); //convert to days
     //output where ui is updated
     setState(() {
-      weeksNeeded = finalWeeks;
-      monthsNeeded = finalMonths;
+      weeksNeeded = fullWeeks;
+      daysNeeded = extraDays;
     });
   }
 }
